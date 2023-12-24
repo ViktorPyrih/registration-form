86f37c3d65979d18c9fc7ac410570a88132dd55a
ViktorPyrih <v24102002>
2023-12-25 01:03:58 +0200

    Added bot protection to block RegistrationForm if there are too many attempts to set an easy password

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -3,10 +3,22 @@ import {useState} from "react";
 
 const PASSWORD_STRENGTH_THRESHOLD = 3;
 
+const ATTEMPTS_THRESHOLD = 3;
+const RESET_ATTEMPTS_MS = 60000;
+
 function RegistrationForm() {
     const [password, setPassword] = useState("");
+    const [attempts, setAttempts] = useState(0);
+
+    const incrementAttempts = () => setAttempts(attempts + 1);
+    const resetAttempts = () => setAttempts(0);
+
+    if (attempts >= ATTEMPTS_THRESHOLD) {
+        setTimeout(() => setAttempts(0), RESET_ATTEMPTS_MS);
+    }
+
     return (
-        <form onSubmit={e => checkPasswordStrength(e, password)}>
+        <form onSubmit={e => checkPasswordStrength(e, password, incrementAttempts, resetAttempts)}>
             <label htmlFor="username">Username:</label>
             <input type="text" id="username" name="username" required/>
 
@@ -16,7 +28,7 @@ function RegistrationForm() {
             <label htmlFor="password">Password:</label>
             <input value={password} type="password" id="password" name="password"
                    onChange={e => setPassword(e.target.value)}
-                   onBlur={e => checkPasswordStrength(e, e.target.value)}
+                   onBlur={e => checkPasswordStrength(e, e.target.value, incrementAttempts, resetAttempts)}
                    required
             />
 
@@ -27,16 +39,19 @@ function RegistrationForm() {
                 <option value="other">Other</option>
             </select>
 
-            <button type="submit">Register</button>
+            <button type="submit" disabled={attempts >= ATTEMPTS_THRESHOLD}>Register</button>
         </form>
     );
 }
 
-function checkPasswordStrength(e, password) {
+function checkPasswordStrength(e, password, incrementAttempts, resetAttempts) {
     const result = zxcvbn(password);
     if (result.score < PASSWORD_STRENGTH_THRESHOLD) {
-        alert(`Password: '${password}' is not strong enough! Warning: ${result.feedback.warning}`);
+        incrementAttempts();
+        alert(`Password: '${password}' is not strong enough! ${result.feedback.warning ? 'Warning: ' + result.feedback.warning : ''}`);
         e.preventDefault();
+    } else {
+        resetAttempts();
     }
 }
 
